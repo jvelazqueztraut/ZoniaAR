0623b99ad7a05bf1d7ab4c8002f57286c00325f1
Javier Velazquez Traut <javier.traut@unit9.com>
2025-05-30 18:39:34 -0300

    Fix for local development

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
+import fs from 'fs';
 
 export default defineConfig({
   plugins: [
@@ -16,6 +17,13 @@ export default defineConfig({
         ]
       : []),
   ],
+  server: {
+    host: '0.0.0.0', // This allows access from other devices on the network
+    https: {
+      key: fs.readFileSync('./certs/key.pem'),
+      cert: fs.readFileSync('./certs/cert.pem'),
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
